a046c9c3bea67f801f8495d241f9a1c1ff6e8bf9
Ayesh Vininda <ayeshgk@gmail.com>
2024-02-16 00:23:26 +0530

    prescriptive learning implement

--- classifier_model.py
+++ classifier_model.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 
+import predictive_coding as pc
+
 torch.manual_seed(1234)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -73,6 +75,7 @@ class transformer(nn.Module):
 		self.tok_emb = nn.Embedding(vocab_size, embeds_size)
 		self.pos_emb = nn.Embedding(block_size, embeds_size)
 		self.block = block()
+		self.pcl = pc.PCLayer()
 		self.ln1 = nn.LayerNorm(embeds_size)
 		self.ln2 = nn.LayerNorm(embeds_size)
 
@@ -98,40 +101,193 @@ class transformer(nn.Module):
 		embedded = embedded + self.pos_emb(torch.arange(T, device=device))
 		output = self.block(embedded)
 		output = output.mean(dim=1)
+		output = self.pcl(output)
 		output = self.classifier_head(output)
 		return output
 
 
-model = transformer()
-if model_loader:
-	model.load_state_dict(torch.load(model_path))
-model.to(device)
-model_loss = nn.BCEWithLogitsLoss()
-model_optimizer = torch.optim.RMSprop(model.parameters(), lr=4e-4)
 
-for epoch in range(epochs):
-	losses = 0
-	for (inputs, targets) in train_data:
-		inputs = inputs.to(device)
-		targets = targets.to(device)
-		output = model(inputs)
-		loss = model_loss(output, targets)
-		model_optimizer.zero_grad()
-		loss.backward()
-		model_optimizer.step()
-		losses += loss.item()
-	print(f'[{epoch}][Train]', losses)
-	model.eval()
-	test_loss = 0
-	passed = 0
-	for (inputs, targets) in test_data:
-		with torch.no_grad():
-			inputs = inputs.to(device)
-			targets = targets.to(device)
-			outputs = model(inputs)
-			if outputs.argmax() == targets.argmax():
-				passed += 1
-	model.train()
-	print(f'[{epoch}][Test]', ', accuracy', passed / len(dataset_y))
+# PCTrainer_kwargs:
+# 	update_x_at: "all"
+# 	optimizer_x_fn: "SGD"
+# 	optimizer_x_kwargs:
+# 		lr: 0.5
+# 	x_lr_discount: 0.5
+# 	x_lr_amplifier: 1.0
 
-torch.save(model.state_dict(), model_path)
+# 	update_p_at: "all"
+# 	optimizer_p_fn: "Adam"
+# 	optimizer_p_kwargs:
+# 		lr:
+# 			grid_search:
+# 				# - 0.005
+# 				# - 0.001
+# 				# - 0.0005
+# 				- 0.00025
+# 				- 0.0001
+# 				- 0.000075
+# 				- 0.00005
+# 				- 0.000025
+# 				- 0.00001
+# 				# - 0.000005
+# 				# - 0.000001
+# 				# - 0.0000005
+# 		weight_decay:
+# 			grid_search:
+# 				# - 0.0
+# 				# - 0.001
+# 				- 0.01
+# 				# - 0.1
+# 				# - 1.0
+# train_on_batch_kwargs:
+# 	is_log_progress: False
+# 	is_return_results_every_t: False
+# 	# debug
+# 	# is_return_results_every_t: True
+# 	is_checking_after_callback_after_t: False
+	
+
+config = {
+	'predictive_coding': {
+		'update_x_at': 'all',
+		'optimizer_x_fn': 'SGD',
+		'optimizer_x_kwargs': {
+			'lr': 0.5,
+		},
+		'x_lr_discount': 0.5,
+		'x_lr_amplifier': 1.0,
+		'update_p_at': 'all',
+		'optimizer_p_fn': 'Adam',
+		'optimizer_p_kwargs': {
+			'lr': 0.00025,
+			'weight_decay': 0.01,
+		},
+	},
+	# 'model': {
+	# 	'embeds_size': embeds_size,
+	# 	'num_heads': num_heads,
+	# 	'head_size': head_size,
+	# 	'block_size': block_size,
+	# 	'num_classes': num_classes,
+	# 	'drop_prob': drop_prob,
+	# },
+	'PCTrainer_kwargs': {
+		'plot_progress_at': '[]',
+		'update_x_at': 'all',
+		'optimizer_x_fn': 'SGD',
+		'optimizer_x_kwargs': {
+			'lr': 0.5,
+		},
+		'x_lr_discount': 0.5,
+		'x_lr_amplifier': 1.0,
+		'update_p_at': 'all',
+		'optimizer_p_fn': 'Adam',
+		'optimizer_p_kwargs': {
+			'lr': 0.00025,
+			'weight_decay': 0.01,
+		},
+	},
+}
+
+class UModel:
+	def __init__(self, config):
+		self.config = config
+		self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+		# self.model = u.create_model(
+		# 	self.config['predictive_coding'],
+		# 	**self.config['model'],
+		# ).to(self.device)
+		self.model = transformer().to(self.device)
+
+
+		self.config['PCTrainer_kwargs']['optimizer_x_fn']=eval(
+			'optim.{}'.format(self.config['PCTrainer_kwargs']['optimizer_x_fn'])
+		)
+		self.config['PCTrainer_kwargs']['optimizer_p_fn']=eval(
+			'optim.{}'.format(self.config['PCTrainer_kwargs']['optimizer_p_fn'])
+		)
+		self.config['PCTrainer_kwargs']['plot_progress_at']=eval(
+			self.config['PCTrainer_kwargs']['plot_progress_at']
+		)
+		self.pc_trainer = pc.PCTrainer(
+			self.model,
+			**self.config['PCTrainer_kwargs'],
+		)
+
+		self.pc_trainer = pc.PCTrainer(
+            self.model,
+            **self.config['PCTrainer_kwargs'],
+        )
+
+		self.model_loss = nn.BCEWithLogitsLoss()
+		self.model_optimizer = torch.optim.RMSprop(self.model.parameters(), lr=4e-4)
+
+	def train(self, train_data, test_data):
+		for epoch in range(epochs):
+			losses = 0
+			for (inputs, targets) in train_data:
+				inputs = inputs.to(device)
+				targets = targets.to(device)
+				output = self.model(inputs)
+				loss = self.model_loss(output, targets)
+				self.model_optimizer.zero_grad()
+				loss.backward()
+				self.model_optimizer.step()
+				losses += loss.item()
+			print(f'[{epoch}][Train]', ', losses', losses)
+			self.model.eval()
+			test_loss = 0
+			passed = 0
+			for (inputs, targets) in test_data:
+				with torch.no_grad():
+					inputs = inputs.to(device)
+					targets = targets.to(device)
+					outputs = self.model(inputs)
+					if outputs.argmax() == targets.argmax():
+						passed += 1
+			self.model.train()
+			print(f'[{epoch}][Test]', ', accuracy', passed / len(dataset_y))
+
+		torch.save(self.model.state_dict(), model_path)
+
+# model = transformer()
+# if model_loader:
+# 	model.load_state_dict(torch.load(model_path))
+# model.to(device)
+		
+# model = 
+
+
+# model_loss = nn.BCEWithLogitsLoss()
+# model_optimizer = torch.optim.RMSprop(model.parameters(), lr=4e-4)
+
+# for epoch in range(epochs):
+# 	losses = 0
+# 	for (inputs, targets) in train_data:
+# 		inputs = inputs.to(device)
+# 		targets = targets.to(device)
+# 		output = model(inputs)
+# 		loss = model_loss(output, targets)
+# 		model_optimizer.zero_grad()
+# 		loss.backward()
+# 		model_optimizer.step()
+# 		losses += loss.item()
+# 	print(f'[{epoch}][Train]', ', losses', losses)
+# 	model.eval()
+# 	test_loss = 0
+# 	passed = 0
+# 	for (inputs, targets) in test_data:
+# 		with torch.no_grad():
+# 			inputs = inputs.to(device)
+# 			targets = targets.to(device)
+# 			outputs = model(inputs)
+# 			if outputs.argmax() == targets.argmax():
+# 				passed += 1
+# 	model.train()
+# 	print(f'[{epoch}][Test]', ', accuracy', passed / len(dataset_y))
+		
+
+		
+umodel = UModel(config)
+umodel.train(train_data, test_data)
+torch.save(umodel.model.state_dict(), model_path)
